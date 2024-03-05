cd47ec5dae7f7454eb78c2b30d9ad3c3590a6507
RoxanaCamara <roxana.camara07@gmail.com>
2024-03-05 10:59:20 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * @format
+ */
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <View>
+      <Text>Hola NAre. cuchaskhasduas</Text>
+    </View>
+  );
+}
+
+
+export default App;
